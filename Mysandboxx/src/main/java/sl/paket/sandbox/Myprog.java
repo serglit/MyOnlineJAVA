47b1b7d4adfd8898c6f376c57a86db5db37e290c
serglit72 <serglit72@gmail.com>
2016-07-21 00:11:18 -0700

    New functions has added

--- Mysandboxx/src/main/java/sl/paket/sandbox/Myprog.java
+++ Mysandboxx/src/main/java/sl/paket/sandbox/Myprog.java
@@ -2,7 +2,26 @@ package sl.paket.sandbox;
 
 public class Myprog {
   public static void main(String[] args) {
-    System.out.println("Hello, JAVA!");
+  hello("Sergei");
+
+      double a = 6;
+      double b = 5;
+      System.out.println("Area ="+ area1(a,b));
+
   }
+    public static void hello (String whoUare) {
+   System.out.println("Hello," + whoUare+ "!");
+
+ }
+
+    public static double area1 (double a, double b) {
+     return a*b;
+
+
+
+    }
+
+
+
 
 }
