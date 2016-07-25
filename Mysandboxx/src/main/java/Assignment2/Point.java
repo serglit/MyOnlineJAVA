96312b2a1883687d975d898630ffd747b14e8bb9
serglit72 <serglit72@gmail.com>
2016-07-24 22:07:30 -0700

    Assignment 2(p.4)

--- Mysandboxx/src/main/java/Assignment2/Point.java
+++ Mysandboxx/src/main/java/Assignment2/Point.java
@@ -6,9 +6,18 @@ package Assignment2;
 public class Point {
     public double x;
     public double y;
-        public Point (double x, double y){
-            this.x = x;
-            this.y = y;
-        }
 
-}
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance(Point p1, Point p2) {
+
+        // double a = p2.x-p1.x; Math.pow (double а,double х) - возведение числа а в степень х)
+        //   double b = p2.y-p1.y; Math.sqrt - извлечение квадратного корня
+
+        return Math.sqrt(Math.pow((p2.x-p1.x),2) + Math.pow((p2.y-p1.y),2));
+
+    }
+    }
