6254e61bfd9c82d8738360cc01ccf9844d57f9d8
serglit <serglit102@gmail.com>
2016-07-29 17:19:42 -0700

    ReCreated Test for Creating Group

--- addressbook-web-tests/src/test/java/sl/paket/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/sl/paket/addressbook/GroupCreationTest.java
@@ -14,20 +14,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 import static org.openqa.selenium.OutputType.*;
 
-public class GroupCreatorTest {
+public class GroupCreationTest {
     FirefoxDriver wd;
     
     @BeforeMethod
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreatorTest() {
         wd.get("http://localhost/addressbook/index.php");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).sendKeys("\\undefined");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
@@ -35,20 +29,25 @@ public class GroupCreatorTest {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void groupCreateTest() {
+
+        wd.findElement(By.id("content")).click();
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("Mygroup1");
+        wd.findElement(By.name("group_name")).sendKeys("MynewGroup2");
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("ThisIsHeader");
+        wd.findElement(By.name("group_header")).sendKeys("HeaderForMyGroup");
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("ThisIsFooter");
+        wd.findElement(By.name("group_footer")).sendKeys("FooterForMyGroup");
         wd.findElement(By.name("submit")).click();
-        wd.findElement(By.linkText("home")).click();
-        wd.findElement(By.linkText("groups")).click();
+        wd.findElement(By.linkText("group page")).click();
     }
     
     @AfterMethod
