89c9cbba753466e1221203ce67dfff064db8edac
serglit <serglit102@gmail.com>
2016-07-31 21:52:26 -0700

    Created TestGroupModification

---
+++ addressbook-web-tests/src/test/java/sl/paket/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package sl.paket.addressbook.tests;
+
+import org.testng.annotations.Test;
+import sl.paket.addressbook.model.GroupData;
+
+/**
+ * Created by serglit on 31.07.16.
+ */
+public class GroupModificationTests extends TestBase{
+    @Test
+    public void testGroupModification(){
+
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillupGroupForm(new GroupData("MynewGroup3", "Header_Group", "Footer_Group"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
