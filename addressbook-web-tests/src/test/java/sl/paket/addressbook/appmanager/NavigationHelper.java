5b27cb6904725ad505c22e2c14b2bcb233f135e4
serglit <serglit102@gmail.com>
2016-08-04 16:09:10 -0700

    NavigationHelper optimized
    Добавлены проверки открытия страницы

--- addressbook-web-tests/src/test/java/sl/paket/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/sl/paket/addressbook/appmanager/NavigationHelper.java
@@ -14,10 +14,19 @@ public class NavigationHelper extends HelperBase{
        super(wd);
     }
     public void goToGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+                return;
+        }
         click(By.linkText("groups"));
     }
 
     public void returnToContactPage() {
+        if (isElementPresent(By.id("maintable"))){
+            return;
+        }
+
         click(By.linkText("home"));
     }
 }
