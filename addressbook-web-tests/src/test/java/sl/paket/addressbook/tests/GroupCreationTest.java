ffb4c220427f47dc70b96d2e3c3ef3fdccecd6ac
serglit <serglit102@gmail.com>
2016-08-09 13:48:34 -0700

    Create Group modify with "stream" and finding MAX identifier

--- addressbook-web-tests/src/test/java/sl/paket/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/sl/paket/addressbook/tests/GroupCreationTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import sl.paket.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -15,10 +16,23 @@ public class GroupCreationTest extends TestBase {
 
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-
-        app.getGroupHelper().createTestGroup(new GroupData("MynewGroup2", "HeaderForMyGroup", "FooterForMyGroup"));
+        GroupData group = new GroupData("MynewGroup2", "HeaderForMyGroup", "FooterForMyGroup");
+        app.getGroupHelper().createTestGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
-    }
 
+        int max = 0;
+        for (GroupData g : after){
+            if(g.getId()>max){
+                max=g.getId();
+            }
+        }
+
+        group.setId(after.stream().max((o1,o2)-> Integer.compare(o1.getId(),o2.getId())).get().getId());
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+    }
 }
+
+
